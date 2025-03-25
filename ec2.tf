c2d468462f9cbec487870abf0b869e31da059208
jithendra150 <jithendranathreddyc@gmail.com>
2025-03-25 23:46:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
     ami = "ami-084568db4383264d4"
-    instance_type = "jenkins"
-    key_name = "t2.micro"
+    instance_type = "t2.micro"
+    key_name = "jenkins"
     tags = {
       Name = "new_instance"
       
